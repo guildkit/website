5586d5003acfea39db9e0263652265a0e2ac76a8
Jumpei Ogawa <git@phanective.org>
2026-04-30 17:34:49 +0900

    chore: add type definition for eslint-plugin-astro

---
+++ src/jsx.d.ts
@@ -0,0 +1,10 @@
+// This file is required for ESLint
+// See: https://ota-meshi.github.io/eslint-plugin-astro/user-guide/#resolving-error-in-jsx-unsafe-return-of-an-any-typed-value
+
+import "astro/astro-jsx";
+
+declare global {
+  namespace JSX {
+    type Element = HTMLElement;
+  }
+}
